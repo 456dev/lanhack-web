5ad176afab84f0237534488bcf982011cecf05f5
MaxHearnden <maxoscarhearnden@gmail.com>
2023-11-25 19:09:01 +0000

    request uids for server

--- frontend/static/main.js
+++ frontend/static/main.js
@@ -1,4 +1,7 @@
 function main() {
 	output = document.getElementById("output")
-  output.innerText = "test data";
+  var xmlHttp = new XMLHttpRequest();
+  xmlHttp.open( "GET", "/api/get-uids", false);
+  xmlHttp.send( null );
+  output.innerText = xmlHttp.responseText;
 }
