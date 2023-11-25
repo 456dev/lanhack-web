38aee862ad272c0e42d90e72bb8a4c2f89278058
The456gamer <the456gamer@the456gamer.dev>
2023-11-25 15:43:35 +0000

    move init for fast api app up: allows using external wsgi servers
    including auto reloading uvicorn, production servers

--- lanhackbackend/main.py
+++ lanhackbackend/main.py
@@ -4,15 +4,14 @@ from fastapi import FastAPI
 from lanhackbackend.api.data.storage import storage
 from lanhackbackend.api.routes.base import api_router
 
+# init fastapi
+app = FastAPI()
+app.include_router(api_router, prefix="/api")
+
+# init storage
+storage.init()
 
 def main():
-    # init fastapi
-    app = FastAPI()
-    app.include_router(api_router, prefix="/api")
-
-    # init storage
-    storage.init()
-
     # run server
     uvicorn.run(app, host="localhost", port=8000)
 
