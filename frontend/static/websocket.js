f266a4cb25abd350b3685f20fb6aaa50caa92b2b
The456gamer <the456gamer@the456gamer.dev>
2023-11-26 11:09:41 +0000

    retry websocket connections (might be funky, idk)

--- frontend/static/websocket.js
+++ frontend/static/websocket.js
@@ -1,7 +1,19 @@
 // Create a new WebSocket connection
-const socket = new WebSocket('ws://' + window.location.host + '/api/ws');
 
-// Add an event listener to the WebSocket connection
-socket.addEventListener('message', (event) => {
-  addUIDToList(JSON.parse(event.data));
-});
+let socket
+
+function setupWebsockets() {
+  socket = new WebSocket('ws://' + window.location.host + '/api/ws');
+  socket.addEventListener('message', (event) => {
+    addUIDToList(JSON.parse(event.data));
+  });
+  socket.addEventListener("close", ev => {
+    setTimeout(setupWebsockets, 1000);
+  })
+  socket.addEventListener("error", ev => {
+    setTimeout(setupWebsockets, 1000);
+
+  })
+}
+
+setupWebsockets()
